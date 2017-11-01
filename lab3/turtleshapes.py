613613083dbf9b17b39869930692ce9c08019453
itamar19-meet <itamar19@meet.mit.edu>
2017-11-01 19:23:25 +0200

    looooooooooool

---
+++ lab3/turtleshapes.py
@@ -0,0 +1,7 @@
+import turtle
+for i in range(5):
+	turtle.forward(120)
+	turtle.left(150)
+
+
+turtle.mainloop()
---
+++ lab3/zucc.py
@@ -0,0 +1,4 @@
+import turtle
+turtle.register_shape("GitGud",((50,0),(50,50),(0,50),(0,0)))
+turtle.addshape("ima")
+turtle.shape("GitGud")
